3d1bcf150e362868478da7de537fd9af15a04dc7
Madhulatha17 <bandimadhulatha4@gmail.com>
2026-06-18 17:16:23 +0530

    Create MyToken.sol

---
+++ contracts/MyToken.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.20;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+
+    constructor(uint256 initialSupply) ERC20("MadhuToken", "MDT") {
+        _mint(msg.sender, initialSupply * 10 ** decimals());
+    }
+}
